450e011a49aec96e2a0bb06ed685eea61ffd11fd
最好喝的小米粥 <admin@cunyx.cn>
2024-01-14 06:31:21 +0000

    神权！
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

---
+++ apps/fix.js
@@ -0,0 +1,26 @@
+import plugin from './../../../lib/plugins/plugin.js';
+export class cunyx_fix extends plugin {
+  constructor () {
+    super({
+      name:"[寸幼萱]开发测试",
+      dsc:"你管我干嘛",
+      event:"message",
+      priority:1,/*优先级*/
+      rule:[
+        {reg:"^#?查看文件(.*)",fnc:"ck"}
+      ]
+    });
+  }
+  async cx (e) {
+    if (!e.isMaster) {
+      e.reply('你有毒吧，真以为自己挺有能耐啊？',true);
+      return false;
+    }
+    try {
+      let path = e.msg.replace(/查看文件|#/g,'');
+      e.reply(fs.readFileSync(process.cwd()+path),true);
+    } catch (err) {
+      e.reply('未找到该文件');
+    }
+  }
+}
