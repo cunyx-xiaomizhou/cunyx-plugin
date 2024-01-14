50e5d729e112506fc51482f6a31e55dc3cbc75f1
最好喝的小米粥 <admin@cunyx.cn>
2024-01-14 07:40:09 +0000

    update apps/bushu.js.
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/bushu.js
+++ apps/bushu.js
@@ -75,7 +75,7 @@ async function write (e,keys,value) {
         return false;
     }
 }
-async function date(format = 'Y-m-d H:i:s.l', timestamp = null) {
+async function date(format, timestamp = null) {
     const currentDate = timestamp ? new Date(timestamp) : new Date();
     const year = currentDate.getFullYear();
     const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
