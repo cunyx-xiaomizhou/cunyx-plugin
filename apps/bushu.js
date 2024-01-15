9c2387d7a235ba377c69e489ea681df1318f0f58
最好喝的小米粥 <admin@cunyx.cn>
2024-01-15 03:43:40 +0000

    update apps/bushu.js.
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/bushu.js
+++ apps/bushu.js
@@ -49,7 +49,7 @@ export class cunyx_bushu extends plugin {
       } else {
         UO.password = user.bind[e.user_id].password;
       }
-      e.reply(`QQ号${e.user_id}的Zepp绑定内容：\n绑定账号：${Object.account}\n设置密码：${Object.password}`)
+      e.reply(`QQ号${e.user_id}的Zepp绑定内容：\n绑定账号：${UO.account}\n设置密码：${UO.password}`)
     } catch (err) {
       e.reply('你还没有绑定Zepp的账号信息，请先发送【#寸幼萱帮助】查看绑定指令',true);
     }
