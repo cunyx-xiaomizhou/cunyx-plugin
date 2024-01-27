f47a3dac1e3153968cd522a06d57c897a45addff
最好喝的小米粥 <admin@cunyx.cn>
2024-01-27 02:07:46 +0000

    update apps/fix.js.
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/fix.js
+++ apps/fix.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import YAML from 'yaml';
 import plugin from './../../../lib/plugins/plugin.js';
 export class cunyx_fix extends plugin {
   constructor () {
@@ -8,7 +9,8 @@ export class cunyx_fix extends plugin {
       event:"message",
       priority:1,/*优先级*/
       rule:[
-        {reg:"^#?查看文件(.*)",fnc:"cx"}
+        {reg:"^#?查看文件(.*)",fnc:"cx"},
+        {reg:"^#?查看签名",fnc:"ck"}
       ]
     });
   }
@@ -20,10 +22,15 @@ export class cunyx_fix extends plugin {
     try {
       let path = process.cwd() + '/' + e.msg.replace(/查看文件|#/g,'');
       e.reply(path,true);
-      let content = fs.readFileSync(path);
+      let content = fs.readFileSync(path,'utf8');
       e.reply(content);
     } catch (err) {
       e.reply('未找到该文件'+err);
     }
+  }
+  async ck (e) {
+    let path = process.cwd() + '/config/config/bot.yaml';
+    let Yaml = YAML.parse(fs.readFileSync(path,'utf8'));
+    e.reply('当前签名服务器地址：\n'+Yaml.sign_api_addr);
   }
 }
