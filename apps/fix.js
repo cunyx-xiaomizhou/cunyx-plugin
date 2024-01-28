32dc0b5bb9c6a394125aeeed041d079b4644516d
最好喝的小米粥 <admin@cunyx.cn>
2024-01-28 01:31:45 +0000

    阿巴巴？
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/fix.js
+++ apps/fix.js
@@ -10,7 +10,8 @@ export class cunyx_fix extends plugin {
       priority:1,/*优先级*/
       rule:[
         {reg:"^#?查看文件(.*)",fnc:"cx"},
-        {reg:"^#?查看签名",fnc:"ck"}
+        {reg:"^#?查看签名",fnc:"ck"},
+        {reg:/^#?帮我(at|艾特|@)(.*)/gi,fnc:"at"}
       ]
     });
   }
@@ -36,5 +37,13 @@ export class cunyx_fix extends plugin {
     let path = process.cwd() + '/config/config/bot.yaml';
     let Yaml = YAML.parse(fs.readFileSync(path,'utf8'));
     e.reply('当前签名服务器地址：\n'+Yaml.sign_api_addr);
+  }
+  async at (e) {
+    let qq = e.msg.replace(/帮我(at|@|艾特)|#/gi, '').trim();
+    try {
+      e.reply(segment.at(qq),true);
+    } catch (err) {
+      e.reply('at失败，原因：\n'+err,true);
+    }
   }
 }
