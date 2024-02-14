93ec742b9d532ee16742d7e01db37f8e30436963
最好喝的小米粥 <admin@cunyx.cn>
2024-02-14 06:53:00 +0000

    写一下QQ获取，等下想办法获得uid
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/profile.js
+++ apps/profile.js
@@ -1,4 +1,8 @@
+import YAML from 'yaml';
+import fs from 'fs';
+import fetch from 'node-fetch';
 import plugin from './../../../lib/plugins/plugin.js';
+let data = YAML.parse(fs.readFileSync('./plugins/cunyx-plugin/config/cunyx_api.yaml','utf-8'));
 export class cunyx_msg extends plugin {
   constructor () {
     super({
@@ -10,5 +14,19 @@ export class cunyx_msg extends plugin {
         {reg:"^#?(原神)?信息",fnc:"gs"}
       ]
     });
+  }
+  async gs (e) {
+    let qq = e.msg.replace(/(原神)?信息|#/g, '').trim();
+    if (qq=='') {
+      qq = e.message.filter(item => item.type == 'at')?.map(item => item?.qq);
+      if (qq=='') {
+        qq=e.user_id;
+      }
+    }
+    try {
+      //
+    } catch (err) {
+        //
+    }
   }
 }
